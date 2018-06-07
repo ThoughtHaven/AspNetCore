27194e2516bb2d876adc7435889f6f9b795831d9
Brendan Ridenour <Journeyman979@hotmail.com>
2018-06-07 04:03:24 -0400

    Add WebAppPagesBase

---
+++ src/ThoughtHaven.AspNetCore.Abstractions/Http/WebAppPagesBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace ThoughtHaven.AspNetCore.Http
+{
+    public abstract class WebAppPagesBase
+    {
+        protected Uri BaseUri { get; }
+
+        protected WebAppPagesBase(Uri baseUri)
+        {
+            this.BaseUri = Guard.Null(nameof(baseUri), baseUri);
+        }
+
+        protected virtual Uri BuildUri(string path, QueryString query = default(QueryString))
+        {
+            if (path != string.Empty) { Guard.NullOrWhiteSpace(nameof(path), path); }
+
+            if (path.EndsWith("/")) { path = path.Substring(0, path.Length - 1); }
+
+            return this.BuildUri(new PathString(path), query);
+        }
+
+        protected virtual Uri BuildUri(PathString path,
+            QueryString query = default(QueryString)) =>
+            new Uri(this.BaseUri, $"{path}{query}");
+    }
+}
---
+++ test/ThoughtHaven.AspNetCore.Abstractions.Test/Http/WebAppPagesBaseTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using ThoughtHaven.AspNetCore.Fakes;
+using Xunit;
+
+namespace ThoughtHaven.AspNetCore.Http
+{
+    public class WebAppPagesBaseTests
+    {
+        public class Constructor
+        {
+            public class BaseUriOverload
+            {
+                [Fact]
+                public void NullBaseDomain_Throws()
+                {
+                    Assert.Throws<ArgumentNullException>("baseUri", () =>
+                    {
+                        new FakeWebAppPagesBase(baseUri: null);
+                    });
+                }
+            }
+        }
+
+        public class BuildUriMethod
+        {
+            public class StringPathAndQueryOverload
+            {
+                [Fact]
+                public void NullPath_Throws()
+                {
+                    Assert.Throws<ArgumentNullException>("path", () =>
+                    {
+                        Pages().BuildUri(path: (string)null);
+                    });
+                }
+
+                [Fact]
+                public void WhiteSpacePath_Throws()
+                {
+                    Assert.Throws<ArgumentException>("path", () =>
+                    {
+                        Pages().BuildUri(path: " ");
+                    });
+                }
+
+                [Fact]
+                public void EmptyPath_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("");
+
+                    Assert.Equal(new Uri("https://example.com/"), result);
+                }
+
+                [Fact]
+                public void EmptyPathAndQuery_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("", new QueryString("?one=1"));
+
+                    Assert.Equal(new Uri("https://example.com/?one=1"), result);
+                }
+
+                [Fact]
+                public void RootPath_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("/");
+
+                    Assert.Equal(new Uri("https://example.com/"), result);
+                }
+
+                [Fact]
+                public void RootPathAndQuery_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("", new QueryString("?one=1"));
+
+                    Assert.Equal(new Uri("https://example.com/?one=1"), result);
+                }
+
+                [Fact]
+                public void WithPath_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("/path");
+
+                    Assert.Equal(new Uri("https://example.com/path"), result);
+                }
+
+                [Fact]
+                public void WithPathAndQuery_ReturnsUri()
+                {
+                    var result = Pages().BuildUri("/path", new QueryString("?one=1"));
+
+                    Assert.Equal(new Uri("https://example.com/path?one=1"), result);
+                }
+            }
+        }
+
+        private static FakeWebAppPagesBase Pages() =>
+            new FakeWebAppPagesBase(new Uri("https://example.com"));
+    }
+}
