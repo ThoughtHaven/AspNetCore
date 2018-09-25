0d15b50f16290f0faba4aa55b8a37f1625f9e741
Brendan Ridenour <Journeyman979@hotmail.com>
2018-09-25 14:35:41 -0400

    Add cache attributes

---
+++ src/ThoughtHaven.AspNetCore.Mvc/Caching/DefaultResponseCacheAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThoughtHaven.AspNetCore.Mvc
+{
+    public class DefaultResponseCacheAttribute : ResponseCacheAttribute
+    {
+        public DefaultResponseCacheAttribute()
+        {
+            this.Duration = 60 * 5;
+            this.Location = ResponseCacheLocation.Any;
+        }
+    }
+}
---
+++ src/ThoughtHaven.AspNetCore.Mvc/Caching/NeverResponseCacheAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThoughtHaven.AspNetCore.Mvc
+{
+    public class NeverResponseCacheAttribute : ResponseCacheAttribute
+    {
+        public NeverResponseCacheAttribute()
+        {
+            this.Location = ResponseCacheLocation.None;
+            this.NoStore = true;
+        }
+    }
+}
