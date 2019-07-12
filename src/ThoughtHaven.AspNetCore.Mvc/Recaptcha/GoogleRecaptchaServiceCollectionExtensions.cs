f670623a2ae10cd55d935eb1e3b73c9e94c58f5a
Brendan Ridenour <Journeyman979@hotmail.com>
2019-07-12 05:58:54 -0400

    Recaptcha service collection extensions

---
+++ src/ThoughtHaven.AspNetCore.Mvc/Recaptcha/GoogleRecaptchaServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ThoughtHaven;
+using ThoughtHaven.AspNetCore.Mvc.Recaptcha;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class GoogleRecaptchaServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGoogleRecaptchaService(
+            this IServiceCollection services, GoogleRecaptchaOptions options)
+        {
+            Guard.Null(nameof(services), services);
+            Guard.Null(nameof(options), options);
+
+            services.AddSingleton(options);
+            services.AddHttpClient<GoogleRecaptchaService>();
+
+            return services;
+        }
+    }
+}
